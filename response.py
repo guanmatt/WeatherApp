c32c89cae4072761fbab4a269ddcb49519430483
guanmatt <56009958+guanmatt@users.noreply.github.com>
2023-03-01 22:58:18 -0500

    Adding error handling

--- response.py
+++ response.py
@@ -1,8 +1,17 @@
 import json
 
+def parse(data):
+    jsonObj = toJsonObject(data)
+    try:
+        if 'error' in jsonObj:
+            raise RuntimeError
+        print(getTempC(data))
+    except:
+        print("**********************************Country not found")
+
 def getTempC(data):
-    data = json.loads(data)
-    return data['current']['temp_c']
+    jsonObj = toJsonObject(data)
+    return "Country: {}, City: {}, Temp: {}C".format(jsonObj['location']['country'], jsonObj['location']['name'], jsonObj['current']['temp_c'])
 
 def toJsonObject(data):
     return json.loads(data)
