ac01823be399239d399e9695c4dc623477c677f0
Carl Zhang <carlzproj@gmail.com>
2023-03-01 23:46:39 -0500

    sort country by temperature

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from response import *
 
 def main():
     data = requestData()
-    parse(data)
+    country_data = parse(data)
+
+    sorted_country_data = sorted(country_data, key=lambda x: x['temperature'])
+
+    print(sorted_country_data)
 
 main()
--- response.py
+++ response.py
@@ -1,16 +1,26 @@
 import json
 
 def parse(data):
+    country_data = []
+
     for jsonObj in data:
         try:
             if 'error' in jsonObj.keys():
                 raise RuntimeError
-            print(getTempC(jsonObj))
+            country_data.append(getTempC(jsonObj))
         except:
             print("**********************************Country not found")
 
+    return country_data
+
 def getTempC(jsonObj):
-    return "Country: {}, City: {}, Temp: {}".format(jsonObj['location']['country'], jsonObj['location']['name'], jsonObj['current']['temp_c'])
+    return {
+        "country": jsonObj['location']['country'],
+        "city": jsonObj['location']['name'],
+        "temperature": float(jsonObj['current']['temp_c'])
+    }
+
+# "Country: {}, City: {}, Temp: {}".format(jsonObj['location']['country'], jsonObj['location']['name'], jsonObj['current']['temp_c'])
 
 def toJsonObject(data):
     return json.loads(data)
