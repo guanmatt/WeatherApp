00c47b63125aefa1ef766e6cb3fe23cb5df6cd62
guanmatt <56009958+guanmatt@users.noreply.github.com>
2023-03-01 23:31:07 -0500

    working api calls

--- response.py
+++ response.py
@@ -1,17 +1,16 @@
 import json
 
 def parse(data):
-    jsonObj = toJsonObject(data)
-    try:
-        if 'error' in jsonObj:
-            raise RuntimeError
-        print(getTempC(data))
-    except:
-        print("**********************************Country not found")
+    for jsonObj in data:
+        try:
+            if 'error' in jsonObj.keys():
+                raise RuntimeError
+            print(getTempC(jsonObj))
+        except:
+            print("**********************************Country not found")
 
-def getTempC(data):
-    jsonObj = toJsonObject(data)
-    return "Country: {}, City: {}, Temp: {}C".format(jsonObj['location']['country'], jsonObj['location']['name'], jsonObj['current']['temp_c'])
+def getTempC(jsonObj):
+    return "Country: {}, City: {}, Temp: {}".format(jsonObj['location']['country'], jsonObj['location']['name'], jsonObj['current']['temp_c'])
 
 def toJsonObject(data):
     return json.loads(data)
